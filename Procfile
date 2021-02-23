efa0212641770a4a53dc49728d3ba560a65125d4
Nadeem Ansari <nadeemansari.vi@gmail.com>
2021-02-23 18:50:17 +0530

    add new file

---
+++ Procfile
@@ -0,0 +1,1 @@
+gunicorn medicastore.wsgi --log-file -
--- medicalstore/settings.py
+++ medicalstore/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import django_heroku
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -23,9 +25,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'brd%z3mvvrh3_7z&x@^-((y1cwns&fnq3l5&*8tm-4i2-(pc$k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['medicastore.herokuapp.com']
 
 
 # Application definition
@@ -43,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -119,8 +122,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
 
 LOGIN_URL = '/ca/login/'
 LOGIN_REDIRECT_URL = '/medical/'
 LOGOUT_REDIRECT_URL = LOGIN_URL
+django_heroku.settings(locals())
