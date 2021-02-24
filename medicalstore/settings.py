7badd2e480bdb5d5c1941b71de3ece2b2c268339
nadeem-ninja <44615000+nadeem-ninja@users.noreply.github.com>
2021-02-24 15:38:34 +0530

    Update settings.py

--- medicalstore/settings.py
+++ medicalstore/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'brd%z3mvvrh3_7z&x@^-((y1cwns&fnq3l5&*8tm-4i2-(pc$k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
