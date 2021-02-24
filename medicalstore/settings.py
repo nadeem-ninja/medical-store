8c687752b2067081752bf8005befdd47d20d7bbb
nadeem-ninja <44615000+nadeem-ninja@users.noreply.github.com>
2021-02-24 16:17:27 +0530

    Update settings.py
    1

--- medicalstore/settings.py
+++ medicalstore/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'brd%z3mvvrh3_7z&x@^-((y1cwns&fnq3l5&*8tm-4i2-(pc$k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-ALLOWED_HOSTS = ['medicastore.herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS = ["herokuapp.com"]
 
 
 # Application definition
